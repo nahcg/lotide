a5ea2cefd3cb276fcb216376c7c485403594a401
nahcg <glor.chan@mail.utoronto.ca>
2023-04-23 17:36:29 -0400

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(input, input2) {
+  var result = true;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== input2[i]) {
+      result = false;
+    } else if (input[i] !== input2[i] && result == false) {
+      result = false;
+    } else if (input[i] !== input2[i]) {
+      result = true;
+    }
+  }
+  return result;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(['a', 'b', 'c'], ['a', 'b', 'c']), true);
+assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
+assertEqual(eqArrays(['1', 2, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3, 'thing'], [1, 2, 3, 'thing2']), false);
