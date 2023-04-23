69b99f8ff978c9dc0d4a1161a71960068641f5bb
nahcg <glor.chan@mail.utoronto.ca>
2023-04-23 17:44:48 -0400

    add assertArraysEqual.js function

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const assertArraysEqual = function(input, input2) {
+  var result = true;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== input2[i]) {
+      result = false;
+    } else if (input[i] !== input2[i] && result == false) {
+      result = false;
+    } else if (input[i] !== input2[i]) {
+      result = true;
+    }
+  }
+  if (result == true) {
+    return "arrays are equal";
+  } else {
+    return "arrays are not equal";
+  }
+};
