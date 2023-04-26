0a8a1912e259ba083ed52037dd4ff908773a2a49
nahcg <glor.chan@mail.utoronto.ca>
2023-04-26 17:11:06 -0400

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  reality: "The Circle"
+};
+
+const findKeyByValue = function(someobject, valuetofind) {
+  for (const key of Object.keys(someobject)) {
+    if (someobject[key] === valuetofind) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Circle"), "reality");
