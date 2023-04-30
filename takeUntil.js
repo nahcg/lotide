47572f623670f810e564a21273fdc61c50e5773e
nahcg <glor.chan@mail.utoronto.ca>
2023-04-30 13:07:13 -0400

    add assert function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,25 @@
+const eqArrays = function(input, input2) {
+  let result = true;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== input2[i]) {
+      result = false;
+    } else if (input[i] === input2[i] && result === false) {
+      result = false;
+    } else if (input[i] === input2[i]) {
+      result = true;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(input, input2) {
+  if (eqArrays(input, input2)) {
+    return `Arrays are the same: [${input}] === [${input2}]`;
+  } else {
+    return `Arrays are not the same: ${input} !== ${input2}]`;
+  }
+};
+
 const takeUntil = function(array, callback) {
   let results = [];
   for (const i of array) {
@@ -12,10 +34,8 @@ const takeUntil = function(array, callback) {
 
 //[ 1, 2, 5, 7, 2 ]
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+console.log(assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]))
 
 //[ 'I\'ve', 'been', 'to', 'Hollywood' ]
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+console.log(assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]));
