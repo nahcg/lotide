62cc887b8d7a0fd01961ac63627850ea80be8bac
nahcg <glor.chan@mail.utoronto.ca>
2023-05-10 15:08:11 -0400

    refactor head.js

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(input) {
   return input.shift();
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['something', 'thing2', 10]), 'something');
-assertEqual(head([10, 'thing2', 100]), 'thing');
+module.exports = head;
