177f06c4e386663d01ae9d947a95ae2dc00dd4e6
nahcg <glor.chan@mail.utoronto.ca>
2023-05-11 15:02:25 -0400

    refactor middle.js

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(input, input2) {
-  var result = true;
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] !== input2[i]) {
-      result = false;
-    } else if (input[i] === input2[i] && result === false) {
-      result = false;
-    } else if (input[i] === input2[i]) {
-      result = true;
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(input, input2) {
-  if (eqArrays(input, input2)) {
-    return `Arrays are the same: [${input}] === [${input2}]`;
-  } else {
-    return `Arrays are not the same: ${input} !== ${input2}]`;
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -30,10 +8,4 @@ const middle = function(array) {
   }
 };
 
-
-console.log(assertArraysEqual(middle([1]), [])) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
