61d06635414c637f0e6ee28e222ef3e131bd8913
nahcg <glor.chan@mail.utoronto.ca>
2023-05-11 20:39:55 -0400

    add mocha / chai

---
+++ test/headTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'something' for ['something', 'thing2', 10]", () => {
+    assert.strictEqual(head(['something', 'thing2', 10]), 'something');
+  });
+  it("returns 10 for [10, 'thing2', 100]", () => {
+    assert.strictEqual(head([10, 'thing2', 100]), 10);
+  });
+});
