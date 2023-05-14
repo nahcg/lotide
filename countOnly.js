998e32833246561f1202760fe31ba286a19bbb15
nahcg <glor.chan@mail.utoronto.ca>
2023-05-14 15:21:09 -0400

    refactor using mocha/chai

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countOnly = function(allItems, itemsToCount) {
   let dict = {};
   for (const name of allItems) {
@@ -21,21 +13,4 @@ const countOnly = function(allItems, itemsToCount) {
   return dict;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
