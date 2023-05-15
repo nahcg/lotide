ac19021f76f3b57cd3ada0bb8f82d7cb3b04044a
nahcg <glor.chan@mail.utoronto.ca>
2023-05-15 12:43:19 -0400

    initial commit

---
+++ test/findKeyTest.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+describe("#findKey", () => {
+  it("returns noma for x => x.stars === 2", () => {
+    assert.strictEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 2), "noma")
+  });
+  it("returns Akaleri for x => x.stars === 3", () => {
+    assert.strictEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 3), "Akaleri")
+  });
+});
+
