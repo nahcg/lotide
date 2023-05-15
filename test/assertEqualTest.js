a842b6a38fcdecf8a8ae19bb4568cb6ebf732cca
nahcg <glor.chan@mail.utoronto.ca>
2023-05-15 15:17:21 -0400

    initial commit

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 3));
+console.log(assertEqual("run", "run"));
