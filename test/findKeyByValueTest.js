7379a897e48aa0c0f3d90396a461c92f02ad1f32
nahcg <glor.chan@mail.utoronto.ca>
2023-05-15 13:24:49 -0400

    initial commit

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const findKeyByValue = require('../findKeyByValue');
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  reality: "The Circle"
+};
+
+describe("#findKeyByValue", () => {
+  it("returns 'drama' for 'The Wire'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama")
+  });
+  it("returns undefined for 'That 70's Show'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined)
+  });
+  it("returns 'reality' for 'The Circle'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Circle"), "reality")
+  });
+});
+
