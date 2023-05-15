f5e1f569a637133c2b7b4b0da636a94ccb4544ca
nahcg <glor.chan@mail.utoronto.ca>
2023-05-15 13:24:30 -0400

    refactor using mocha/chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,18 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  reality: "The Circle"
-};
-
 const findKeyByValue = function(someobject, valuetofind) {
   for (const key of Object.keys(someobject)) {
     if (someobject[key] === valuetofind) {
@@ -21,6 +6,4 @@ const findKeyByValue = function(someobject, valuetofind) {
   }
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Circle"), "reality");
+module.exports = findKeyByValue;
