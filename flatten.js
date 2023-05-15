ad7919d45380c9a53c552910e38ffa1ab89d07db
nahcg <glor.chan@mail.utoronto.ca>
2023-05-15 13:57:31 -0400

    refactor using mocha/chai

--- flatten.js
+++ flatten.js
@@ -1,21 +1,3 @@
-const assertArraysEqual = function(input, input2) {
-  let result = true;
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] !== input2[i]) {
-      result = false;
-    } else if (input[i] === input2[i] && result === false) {
-      result = false;
-    } else if (input[i] === input2[i]) {
-      result = true;
-    }
-  }
-  if (result === true) {
-    return "arrays are equal";
-  } else {
-    return "arrays are not equal";
-  }
-};
-
 const flatten = function(inputarray) {
   let result = [];
   for (const item of inputarray) {
@@ -30,5 +12,4 @@ const flatten = function(inputarray) {
   return result;
 };
 
-console.log(flatten([1, 2, ['thing', 'thing2'], 5, [7, 10], 11]));
-console.log(assertArraysEqual((flatten([1, 2, [3, 4], 5, [6]])), [1, 2, 3, 4, 5, 6])); // arrays are equal
+module.exports = flatten;
