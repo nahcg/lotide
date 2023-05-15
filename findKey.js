f6c806dd6ded2c54220e437d32cffd8a16577f45
nahcg <glor.chan@mail.utoronto.ca>
2023-05-15 10:58:26 -0400

    refactor using mocha/chai

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
 const findKey = function(object, callback) {
   var keys = Object.keys(object);
   for (const k of keys) {
@@ -15,20 +7,4 @@ const findKey = function(object, callback) {
   }
 };
 
-console.log(assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma")); 
-
-console.log(assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 3), "Akaleri")); 
+module.exports = findKey;
