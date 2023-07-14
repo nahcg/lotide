ca0a2ddf62a1e263696629149652d276bb8c8e32
nahcg <glor.chan@mail.utoronto.ca>
2023-07-13 20:48:27 -0400

    add comment to describe import / export and how function uses imported function to compare arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,7 @@
+// import eqArrays to compare arrays
 const eqArrays = require('./eqArrays');
 
+// uses eqArray function to compare arrays , sserts equal if given array equal expected array
 const assertArraysEqual = function(input, input2) {
   if (eqArrays(input, input2)) {
     return `Arrays are the same: [${input}] === [${input2}]`;
@@ -8,4 +10,5 @@ const assertArraysEqual = function(input, input2) {
   }
 };
 
+// export function to use for mocha tests
 module.exports = assertArraysEqual;
