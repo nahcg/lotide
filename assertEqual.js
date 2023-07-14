1a51d3c39c4b493fda6d599d5b6f1cf9812d4ab1
nahcg <glor.chan@mail.utoronto.ca>
2023-07-13 20:49:16 -0400

    add comment to describe function comparing variable to variable

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+// function returns assertion pass or fail if given variable equals expected variable, does not work for arrays or objects
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return `Assertion Passed: ${actual} === ${expected}`;
